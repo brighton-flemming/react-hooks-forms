35f438c82b25c9e6fadf6258d80f1d25c86b0856
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-23 23:36:40 +0300

    Mission Passed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,45 @@
 import React, { useState } from "react";
 
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
+
+//   function handleFirstNameChange(event) {
+//     setFirstName(event.target.value);
+//   }
+  
+//   function handleLastNameChange(event) {
+//     setLastName(event.target.value);
+//   }
+
+
+//   return (
+//     <form>
+//       <input onChange={handleFirstNameChange} type="text" value={firstName} />
+//       <input onChange={handleLastNameChange} type="text" value={lastName} />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [newsletter, setNewsletter] = useState(false);
+
+  function handleNewsletterChange(event) {
+    // .checked, not .value!
+    setNewsletter(event.target.checked);
+  }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input
+        type="checkbox"
+        id="newsletter"
+        onChange={handleNewsletterChange}
+        checked={newsletter}
+      />
       <button type="submit">Submit</button>
     </form>
   );
 }
-
 export default Form;
